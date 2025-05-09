82e527770025326b7bca59d9e18f5e0c23a87272
Nayan Shirpure <nayanshirpure@gmail.com>
2025-05-09 23:06:03 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,6 @@
 
 import type { MetadataRoute } from 'next';
-
+export const dynamic = 'force-static'
 // IMPORTANT: Set NEXT_PUBLIC_SITE_URL in your environment variables (e.g., .env.local or Vercel/Netlify settings)
 // Example: NEXT_PUBLIC_SITE_URL=https://www.yourdomain.com
 const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || 'https://wallify.example.com';

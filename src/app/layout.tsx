193cfd6805cb0c3f8c8c53fb7f8471924c2c6b2b
Nayan Shirpure <nayanshirpure@gmail.com>
2025-05-13 02:15:02 +0530

    google-site-verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -104,6 +104,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
          <StructuredData data={webSiteSchema} />
+         <meta name="google-site-verification" content="apm8EU8qRvhCz7XYwbXyTM3JQAu4whs85UL0HaZCnDc" />
       </head>
       <body className={cn(
         inter.variable,
